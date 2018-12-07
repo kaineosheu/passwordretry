df4841d2c1394d11a5d53dc41de7c9064125f0ef
kaineosheu <kaineosheu@gmail.com>
2018-12-07 12:02:19 +0800

    updated while condition twice

--- test1.py
+++ test1.py
@@ -1,14 +1,19 @@
 pw = 'a123456'
-count = 0
-total = 3
-while total > 0:
+count = 3
+while count > 0:
+	count = count - 1
 	input_pw = input('請輸入密碼登入，最多輸入3次密碼: ')
-	count = count + 1
 	if pw == input_pw:
 		print('登入成功')
 		break
-	total = total - count
-	print('密碼錯誤解 還有',total,'機會')
+	else:
+		if count > 0:
+			print('密碼錯誤解 還有',count,'機會')
+		else:
+			print('密碼錯誤')
+			print('沒有機會嘗試! 要鎖帳號了拉')
+		
+	
 	
 
 
